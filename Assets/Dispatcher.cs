545a88be995e8df04d9d413513bb46b9775b7540
kalle (jag) <kalle.jillheden@gmail.com>
2018-12-15 18:10:51 +0100

    Added testing scripts

---
+++ Assets/Dispatcher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dispatcher : MonoBehaviour
+{
+    private static Dispatcher _singleton;
+
+    private readonly ConcurrentBag<Action> actions = new ConcurrentBag<Action>();
+
+    private void OnEnable()
+    {
+        _singleton = this;
+    }
+
+    private void Update()
+    {
+        if (actions.IsEmpty) return;
+
+        while (actions.TryTake(out Action action))
+        {
+            action();
+        }
+    }
+
+    public static void Invoke(Action action)
+    {
+        _singleton.actions.Add(action);
+    }
+}
