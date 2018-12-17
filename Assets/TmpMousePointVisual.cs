c425e673310c024aceb263b43410d7db61c9eef4
kalle (jag) <kalle.jillheden@me.com>
2018-12-17 16:20:11 +0100

    Fixed the tmp mouse pointer visual
    This is kinda base for the input manager i have in mind...
    Not feature creep! I think.

--- Assets/TmpMousePointVisual.cs
+++ Assets/TmpMousePointVisual.cs
@@ -5,12 +5,41 @@ public class TmpMousePointVisual : MonoBehaviour
 {
     public GameObject quad;
 
+    public Color selectedMeshColor = Color.red;
+    public Color unselectedMeshColor = new Color(1, 0, 0, 0.5f);
+
+    [SerializeField] private TextMesh lastTextX;
+    [SerializeField] private TextMesh lastTextY;
     private Plane plane = new Plane(Vector3.up, Vector3.zero);
-    private new Camera camera;
+    [SerializeField] private new Camera camera;
+
+    [SerializeField] private TextMesh[] textMeshesX;
+    [SerializeField] private TextMesh[] textMeshesY;
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.TransformPoint(5, 0 , 5), transform.TransformVector(10, 0, 10));
+    }
 
     private void Awake()
     {
         camera = Camera.main;
+
+        textMeshesX = new TextMesh[10];
+        for (var x = 0; x < 10; x++)
+        {
+            textMeshesX[x] = GameObject.Find((x + 1).ToString()).GetComponent<TextMesh>();
+            textMeshesX[x].color = unselectedMeshColor;
+        }
+
+        const string letters = "ABCDEFGHIJ";
+        textMeshesY = new TextMesh[10];
+        for (var y = 0; y < 10; y++)
+        {
+            textMeshesY[y] = GameObject.Find(letters[y].ToString()).GetComponent<TextMesh>();
+            textMeshesY[y].color = unselectedMeshColor;
+        }
     }
 
     private void Update()
@@ -21,18 +50,28 @@ public class TmpMousePointVisual : MonoBehaviour
         Ray ray = camera.ScreenPointToRay(Input.mousePosition);
         if (plane.Raycast(ray, out float dist))
         {
-            Vector3Int pos = GetPosition(ray.GetPoint(dist));
+            Vector2Int pos = GetWorldToBoard(ray.GetPoint(dist));
+            print(pos);
 
-            if (Board.IsOnBoard(pos.x, pos.z))
+            if (Board.IsOnBoard(pos.x, pos.y))
             {
-                quad.transform.position = new Vector3(pos.x + 0.5f, 0, pos.z + 0.5f);
+                quad.transform.position = GetBoardToWorld(pos, quad.transform.position.y);
 
                 if (!quad.activeSelf)
                     quad.SetActive(true);
 
+                TextMesh textX = textMeshesX[pos.x];
+                TextMesh textY = textMeshesY[pos.y];
+                
+                SetTextMeshAlpha(ref lastTextX, textX);
+                SetTextMeshAlpha(ref lastTextY, textY);
+
                 return;
             }
         }
+
+        SetTextMeshAlpha(ref lastTextX, null);
+        SetTextMeshAlpha(ref lastTextY, null);
 
         if (quad.activeSelf)
         {
@@ -40,8 +79,34 @@ public class TmpMousePointVisual : MonoBehaviour
         }
     }
 
-    private static Vector3Int GetPosition(Vector3 pos)
+    private void SetTextMeshAlpha(ref TextMesh oldMesh, TextMesh newMesh)
     {
-        return Vector3Int.FloorToInt(new Vector3(pos.x, 0, pos.z));
+        if (oldMesh == newMesh)
+            return;
+
+        if (oldMesh != null)
+        {
+            oldMesh.color = unselectedMeshColor;
+        }
+
+        if (newMesh != null)
+        {
+            newMesh.color = selectedMeshColor;
+        }
+
+        oldMesh = newMesh;
+    }
+
+    private Vector2Int GetWorldToBoard(Vector3 pos)
+    {
+        Vector3 inverseTransformPoint = transform.InverseTransformPoint(pos);
+        return Vector2Int.FloorToInt(new Vector2(inverseTransformPoint.x, inverseTransformPoint.z));
+    }
+
+    private Vector3 GetBoardToWorld(Vector2Int pos, float y = 0)
+    {
+        Vector3 point = transform.TransformPoint(new Vector3(pos.x + 0.5f, 0, pos.y + 0.5f));
+        point.y = y;
+        return point;
     }
 }
