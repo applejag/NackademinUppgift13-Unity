555f7218ffa6272045741ec7037eb573445ecf1e
kalle (jag) <kalle.jillheden@me.com>
2018-12-19 17:16:20 +0100

    Added host and join functions

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,20 +1,66 @@
-﻿using System.Globalization;
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
 using BattleshipProtocol;
+using BattleshipProtocol.Protocol;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private BattleGame game;
-    
+    public GameBoard board;
+
+    [NonSerialized]
+    public BattleGame game;
+    private CancellationTokenSource connectCancellationToken;
+    private CancellationTokenSource hostCancellationTokenSource;
+
+    public async Task JoinGame(string address, ushort port, string localPlayerName)
+    {
+        connectCancellationToken = new CancellationTokenSource();
+
+        game = await BattleGame.ConnectAsync(new ConnectionSettings
+        {
+            Address = address,
+            Port = port
+        }, board.protocolBoard, localPlayerName, connectCancellationToken.Token);
+    }
+
+    public void CancelConnect()
+    {
+        connectCancellationToken?.Cancel();
+        connectCancellationToken = null;
+        hostCancellationTokenSource?.Cancel();
+        hostCancellationTokenSource = null;
+        game?.Dispose();
+        game = null;
+    }
+
+    public async Task HostGame(ushort port, string playerName, string localPlayerName)
+    {
+        if (hostCancellationTokenSource != null)
+            throw new InvalidOperationException("Game is already hosting.");
+
+        hostCancellationTokenSource = new CancellationTokenSource();
+
+        game = await BattleGame.HostAndWaitAsync(new ConnectionSettings
+        {
+            Port = port
+        }, board.protocolBoard, localPlayerName, hostCancellationTokenSource.Token);
+    }
+
     private void OnEnable()
     {
         CultureInfo.DefaultThreadCurrentCulture =
         CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.GetCultureInfo("en-US");
     }
-    
-    private void OnDestroy()
+
+    private void OnDisable()
     {
+        connectCancellationToken?.Dispose();
+        connectCancellationToken = null;
         game?.Disconnect();
         game?.Dispose();
+        game = null;
     }
 }
--- Assets/Scripts/GameMenu.cs
+++ Assets/Scripts/GameMenu.cs
@@ -2,6 +2,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +12,8 @@ public class GameMenu : MonoBehaviour
 {
     [Header("Game manager stuff")]
     public string localPlayerName;
+
+    public GameManager gameManager;
 
     [Header("Settings")]
     public float fadeSpeed = 3;
@@ -44,47 +49,60 @@ public class GameMenu : MonoBehaviour
     public Text textPlayerName;
     public Button buttonFinishMovingShips;
     public Text textErrorMessage;
+    public Text textLoadingHeader;
 
-    [SerializeField, HideInInspector]
-    private HashSet<CanvasGroup> currentlyFading = new HashSet<CanvasGroup>();
+    private Dictionary<CanvasGroup, CancellationTokenSource> currentlyFading = new Dictionary<CanvasGroup, CancellationTokenSource>();
 
     private IEnumerator Start()
     {
         yield return new WaitForSeconds(0.2f);
-        StartCoroutine(FadeInMenuCoroutine(groupBackground));
+        StartCoroutine(FadeInMenuCoroutine(groupBackground, fadeSpeed * 0.5f, GetTokenAndCancel(groupBackground)));
         yield return new WaitForSeconds(1);
-        StartCoroutine(FadeInMenuCoroutine(groupPickName));
+        StartCoroutine(FadeInMenuCoroutine(groupPickName, fadeSpeed, GetTokenAndCancel(groupPickName)));
     }
 
     public void FadeInMenu(CanvasGroup canvasGroup)
     {
-        StartCoroutine(FadeInMenuCoroutine(canvasGroup));
+        StartCoroutine(FadeInMenuCoroutine(canvasGroup, fadeSpeed, GetTokenAndCancel(canvasGroup)));
     }
+
     public void FadeOutMenu(CanvasGroup canvasGroup)
     {
-        StartCoroutine(FadeOutMenuCoroutine(canvasGroup));
+        StartCoroutine(FadeOutMenuCoroutine(canvasGroup, fadeSpeed, GetTokenAndCancel(canvasGroup)));
     }
 
-    private bool TryLock(CanvasGroup canvasGroup)
+    private CancellationToken GetTokenAndCancel(CanvasGroup canvasGroup)
     {
-        return currentlyFading.Add(canvasGroup);
+        if (currentlyFading.TryGetValue(canvasGroup, out CancellationTokenSource oldTokenSource))
+        {
+            oldTokenSource.Cancel();
+            oldTokenSource.Dispose();
+        }
+
+        var tokenSource = new CancellationTokenSource();
+        currentlyFading[canvasGroup] = tokenSource;
+        return tokenSource.Token;
     }
 
-    private bool Unlock(CanvasGroup canvasGroup)
+    private void DisposeMySource(CanvasGroup canvasGroup, CancellationToken myToken)
     {
-        return currentlyFading.Remove(canvasGroup);
+        if (currentlyFading.TryGetValue(canvasGroup, out CancellationTokenSource oldTokenSource)
+            && oldTokenSource.Token == myToken)
+        {
+            oldTokenSource.Cancel();
+            oldTokenSource.Dispose();
+            currentlyFading.Remove(canvasGroup);
+        }
     }
 
-    private IEnumerator FadeInMenuCoroutine(CanvasGroup canvasGroup)
+    private IEnumerator FadeInMenuCoroutine(CanvasGroup canvasGroup, float speed, CancellationToken cancellationToken)
     {
-        while (!TryLock(canvasGroup))
-            yield return null;
-
         canvasGroup.GetComponent<MenuGroupEvents>()?.OnFadeInStart.Invoke();
         canvasGroup.gameObject.SetActive(true);
         canvasGroup.blocksRaycasts = true;
 
-        while ((canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 1, Time.deltaTime * fadeSpeed)) < fadingThreshold)
+        while (!cancellationToken.IsCancellationRequested &&
+               (canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 1, Time.deltaTime * speed)) < fadingThreshold)
         {
             canvasGroup.interactable = canvasGroup.alpha > interactableThreshold;
             yield return null;
@@ -94,28 +112,26 @@ public class GameMenu : MonoBehaviour
         canvasGroup.alpha = 1;
         canvasGroup.GetComponent<MenuGroupEvents>()?.OnFadeInEnd.Invoke();
 
-        Unlock(canvasGroup);
+        DisposeMySource(canvasGroup, cancellationToken);
     }
 
-    private IEnumerator FadeOutMenuCoroutine(CanvasGroup canvasGroup)
+    private IEnumerator FadeOutMenuCoroutine(CanvasGroup canvasGroup, float speed, CancellationToken cancellationToken)
     {
-        while (!TryLock(canvasGroup))
-            yield return null;
-
         canvasGroup.GetComponent<MenuGroupEvents>()?.OnFadeOutStart.Invoke();
         canvasGroup.interactable = false;
-        while ((canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 0, Time.deltaTime * fadeSpeed)) > 1 - fadingThreshold)
+        while (!cancellationToken.IsCancellationRequested &&
+               (canvasGroup.alpha = Mathf.Lerp(canvasGroup.alpha, 0, Time.deltaTime * speed)) > 1 - fadingThreshold)
         {
             canvasGroup.blocksRaycasts = canvasGroup.alpha > blockRaycastThreshold;
             yield return null;
         }
-        
+
         canvasGroup.blocksRaycasts = false;
         canvasGroup.alpha = 0;
         canvasGroup.gameObject.SetActive(false);
         canvasGroup.GetComponent<MenuGroupEvents>()?.OnFadeOutEnd.Invoke();
 
-        Unlock(canvasGroup);
+        DisposeMySource(canvasGroup, cancellationToken);
     }
 
     public void Menu_PickName_SetPlayerName()
@@ -138,7 +154,7 @@ public class GameMenu : MonoBehaviour
 
     public void Menu_PickName_CancelSetName()
     {
-        if (localPlayerName.Length == 0)        
+        if (localPlayerName.Length == 0)
         {
             Menu_Error_Show("Please enter your name.");
         }
@@ -163,5 +179,112 @@ public class GameMenu : MonoBehaviour
     {
         textErrorMessage.text = error;
         FadeInMenu(groupErrorModalWithClose);
+    }
+
+    public void Menu_Loading_Show(string header)
+    {
+        textLoadingHeader.text = header;
+        FadeInMenu(groupLoadingModalWithAbort);
+    }
+
+    public void Menu_JoinGame_Connect()
+    {
+        if (!TryGetAddressField(fieldJoinAddress, out string address)) return;
+        if (!TryGetPortField(fieldJoinPort, out ushort port)) return;
+
+        FadeInMenu(groupLoadingModalWithAbort);
+
+        gameManager.JoinGame(address, port, localPlayerName)
+            .ContinueWith(task =>
+            {
+                Dispatcher.Invoke(FadeOutMenu, groupLoadingModalWithAbort);
+
+                if (task.IsFaulted || task.IsCanceled)
+                {
+                    if (task.Exception != null)
+                        Dispatcher.Invoke(Menu_Error_Show, $"Error while connecting to game.\n{task.Exception?.Message}");
+                }
+                else
+                {
+                    Dispatcher.Invoke(FadeInMenu, groupGameStarted);
+                }
+            });
+    }
+
+    public void Menu_Loading_Abort()
+    {
+        gameManager.CancelConnect();
+    }
+
+    public void Menu_HostGame_Host()
+    {
+        if (!TryGetAddressField(fieldJoinAddress, out string address)) return;
+        if (!TryGetPortField(fieldJoinPort, out ushort port)) return;
+
+        FadeInMenu(groupLoadingModalWithAbort);
+
+        gameManager.JoinGame(address, port, localPlayerName)
+            .ContinueWith(task =>
+            {
+                Dispatcher.Invoke(FadeOutMenu, groupLoadingModalWithAbort);
+
+                if (task.IsFaulted || task.IsCanceled)
+                {
+                    if (task.Exception != null)
+                        Dispatcher.Invoke(Menu_Error_Show, $"Error while connecting to game.\n{task.Exception?.Message}");
+                }
+                else
+                {
+                    Dispatcher.Invoke(FadeInMenu, groupGameStarted);
+                }
+            });
+    }
+
+    private bool TryGetAddressField(InputField field, out string address)
+    {
+        address = string.Empty;
+        string input = field.text.Trim();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Menu_Error_Show("The address field is required.");
+            return false;
+        }
+
+        address = input;
+        return true;
+    }
+
+    private bool TryGetPortField(InputField field, out ushort port)
+    {
+        port = default;
+        string input = field.text.Trim();
+
+        if (string.IsNullOrEmpty(input))
+        {
+            Menu_Error_Show("The port field is required.");
+            return false;
+        }
+
+        if (!int.TryParse(input, out int result))
+        {
+            Menu_Error_Show($"Sorry, but \"{result}\" is an invalid format for port.\nOnly digits allowed.");
+            return false;
+        }
+
+        if (result > ushort.MaxValue)
+        {
+            Menu_Error_Show($"Sorry, but port can be at max {ushort.MaxValue}.");
+            return false;
+        }
+
+        if (result < 0)
+        {
+            Menu_Error_Show($"Sorry, but port cannot be negative. Maybe try {-result}");
+            return false;
+        }
+
+        port = (ushort)result;
+        return true;
     }
 }
