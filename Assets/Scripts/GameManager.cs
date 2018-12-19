1a6fd99d86448fc58e2d2c663a22af5f1b1d54b7
kalle (jag) <kalle.jillheden@me.com>
2018-12-19 02:52:10 +0100

    Removed most of game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Globalization;
+using BattleshipProtocol;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private BattleGame game;
+    
+    private void OnEnable()
+    {
+        CultureInfo.DefaultThreadCurrentCulture =
+        CultureInfo.DefaultThreadCurrentUICulture = CultureInfo.GetCultureInfo("en-US");
+    }
+    
+    private void OnDestroy()
+    {
+        game?.Disconnect();
+        game?.Dispose();
+    }
+}
