f185f291f12450f585c11c787ee3ffa1da5aeacd
kalle (jag) <kalle.jillheden@me.com>
2018-12-19 19:46:49 +0100

    More dispatcher helper functions

--- Assets/Scripts/Dispatcher.cs
+++ Assets/Scripts/Dispatcher.cs
@@ -25,6 +25,26 @@ public class Dispatcher : MonoBehaviour
         }
     }
 
+    public static Action Wrap(Action action)
+    {
+        return delegate { Invoke(action); };
+    }
+
+    public static Action<T1> Wrap<T1>(Action<T1> action)
+    {
+        return delegate (T1 param1) { Invoke(action, param1); };
+    }
+
+    public static Action<T1, T2> Wrap<T1, T2>(Action<T1, T2> action)
+    {
+        return delegate (T1 param1, T2 param2) { Invoke(action, param1, param2); };
+    }
+
+    public static Action<T1, T2, T3> Wrap<T1, T2, T3>(Action<T1, T2, T3> action)
+    {
+        return delegate (T1 param1, T2 param2, T3 param3) { Invoke(action, param1, param2, param3); };
+    }
+
     public static void Invoke(Action action)
     {
         _singleton.actions.Add(action);
