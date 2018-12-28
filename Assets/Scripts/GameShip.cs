c32dbf23566e82f0c1fb48f45ed1f899efebaee9
kalle (jag) <kalle.jillheden@me.com>
2018-12-28 20:09:53 +0100

    Added missile silo on each ship

--- Assets/Scripts/GameShip.cs
+++ Assets/Scripts/GameShip.cs
@@ -10,6 +10,22 @@ public class GameShip : MonoBehaviour
     public ShipType shipType;
     public Vector3 offset;
 
+    public Vector3 missileSilo;
+
+#if UNITY_EDITOR
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(GetMissileSiloWorldPosition(), 0.5f);
+        Gizmos.DrawRay(GetMissileSiloWorldPosition() + Vector3.up * 0.5f, Vector3.up * 5);
+    }
+#endif
+
+    public Vector3 GetMissileSiloWorldPosition()
+    {
+        return transform.TransformPoint(missileSilo);
+    }
+
     public void SetPositionFromCoordinate(Vector2Int coordinate, Orientation orientation, float y = 0f)
     {
         Vector3 world = board.CoordinateToWorld(coordinate, y);
